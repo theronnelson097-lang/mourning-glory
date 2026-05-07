3b06201d01b94e9a672858f38a280a91832f2c69
deuce 🏅 <deuce@deuces-MBP.lan>
2026-05-06 21:55:53 -0400

    initial commit

--- app/page.js
+++ app/page.js
@@ -69,7 +69,7 @@ export default function Home() {
           fontFamily: "'AlteHaas', sans-serif",
           letterSpacing: "0.04em"
         }}>
-          Mourning Glory
+          MOURNING GLORY
         </span>
         <button
           onClick={() => setDrawerOpen(true)}
